97513ffa2267dfa1f94e11424bc27399d5118139
Vadironx <131769853+Vadironx@users.noreply.github.com>
2023-08-22 16:35:19 +0300

    PokemonsSearch.php

--- frontend/models/PokemonsSearch.php
+++ frontend/models/PokemonsSearch.php
@@ -5,11 +5,11 @@ namespace frontend\models;
 use Yii;
 use yii\base\Model;
 use common\models\Pokemons;
-// use common\models\Type;
 use yii\data\ActiveDataProvider;
 
 class PokemonsSearch extends Pokemons
 {
+    public $type;
     public function rules()
     {
         
@@ -26,16 +26,7 @@ class PokemonsSearch extends Pokemons
         return Model::scenarios();
     }
 
-    // public $typeTittle;
 
-    // public function afterSave($insert, $changedAttributes) {
-    //     $type = new Type();
-    //     $type->title = $this->$typeTittle;
-    //     $type->pokemons_id = $this->id;
-    //     $type->save();
-    //     parent::afterSave($insert, $changedAttributes);
-
-    // }
 
     public function search($params)
     {
@@ -51,46 +42,12 @@ class PokemonsSearch extends Pokemons
         if (!($this->load($params) && $this->validate())) {
             return $dataProvider;
         }
-
         
-        $query->andFilterWhere(['id' => $this->id]);
+        $query->andFilterWhere(['pokemons.id' => $this->id]);
         $query->andFilterWhere(['like', 'name', $this->name]);
-        $query->andFilterWhere(['like', 'type', $this->type]);
+        $query->andFilterWhere(['like', 'type.title', $this->type]);
 
 
         return $dataProvider;
     }
 }
-
-/*class PokemonSearchForm extends Model
-{
-public function search()
-{
-    $searchModel = new PokemonSearch();
-    $dataProvider = new ActiveDataProvider([
-        'query' => Pokemons::find(),
-        'pagination' => [
-            'pageSize' => 20,
-        ],
-        $searchModel->search(yii::$app->request->get()),
-    ]);
-    return $this->render('index', [
-        'dataProvider' => $dataProvider, 
-        'searchModel' => $searchModel,
-    ]);
-    /*$searchModel = new PokemonSearch();
-    dump(yii::$app->request->get());
-    $provider =  $searchModel->search(yii::$app->request->get());
-    return $this->render(view: 'index', [
-        'provider' => $provider, 
-        'searchModel' => $searchModel,
-    ]);
-    /*$dataProvider = new ActiveDataProvider([
-        'query' => Pokemons::find(),
-        'pagination' => [
-            'pageSize' => 20,
-        ],
-    ]);
-    return $this->render('index', ['dataProvider'=> $dataProvider]);
-}
-}*/
